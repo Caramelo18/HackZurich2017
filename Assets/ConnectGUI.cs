fccfb489e9f03ed8bb2f17f3bea571eb604bbc01
Caramelo18 <fabiocaramelo18@gmail.com>
2017-09-16 10:37:46 +0200

    throttle and braking working (might need some fixes)

--- Assets/ConnectGUI.cs
+++ Assets/ConnectGUI.cs
@@ -20,10 +20,12 @@ public class ConnectGUI : MonoBehaviour {
 
     Vehicle vehicle;
     bool isConnected;
+    float throttle, brake;
 
     // Use this for initialization
     void Start () 
     {
+        Screen.orientation = ScreenOrientation.LandscapeRight;
         imageTexture = new Texture2D(255, 255);
         //panel = new Transform(123, 0, 1);
         //accelerator.transform.SetParent(panel) ;
@@ -42,7 +44,8 @@ public class ConnectGUI : MonoBehaviour {
 
             if (vehicle.Connected)
             {
-                vehicle.SetThrottle(Input.GetAxis("Vertical"));
+                //vehicle.SetThrottle(Input.GetAxis("Vertical"));
+                vehicle.SetThrottle(throttle);
                 vehicle.SetBrake(Mathf.Clamp01(-Input.GetAxis("Vertical")));
                 vehicle.SetSteeringAngle(Input.GetAxis("Horizontal"));
                 vehicle.Update();
@@ -65,10 +68,22 @@ public class ConnectGUI : MonoBehaviour {
     {
         if (!isConnected)
         {
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 200, Screen.height * 0.5f - 40, 400, 80), "Connect"))
+            if (GUI.Button(new Rect(Screen.width * 0.5f - 300, Screen.height * 0.5f - 100, 600, 200), "Connect"))
             {
                 vehicle = Kopernikus.Instance.Vehicle("172.30.6.145");
+                //vehicle = Kopernikus.Instance.Vehicle("192.168.137.193");
             }
         }
+    }
+
+
+    public void Throttle_Changed(float newValue)
+    {
+        this.throttle = newValue;
+    }
+
+    public void Brake_Changed(float newValue)
+    {
+        this.brake = newValue;
     }
 }
