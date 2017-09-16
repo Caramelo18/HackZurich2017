c826dd8d1b55b7d13ff36d852882b71924eaa9ba
William <wnfukunaga@gmail.com>
2017-09-16 08:23:37 +0200

    Added initial files

---
+++ Assets/CustomButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour, IPointerDownHandler
+{
+	public CustomButton()
+	{
+
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(" Was Clicked.");
+    }
+}
